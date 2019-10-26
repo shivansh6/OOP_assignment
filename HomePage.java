3232bbbf226fe4de96063ac862143595e78140b4
shivansh6 <51433148+shivansh6@users.noreply.github.com>
2019-10-26 12:23:55 +0530

    Add files via upload

---
+++ HomePage.java
@@ -0,0 +1,18 @@
+public class HomePage
+{
+  public static void main(String args[])
+  {
+    System.out.println("____________________________________________________________________________________________________________________________________________________________________________");
+    for(int i=0;i<75;i++)
+      System.out.print(" ");
+    System.out.println("Nirma university");
+    for(int i=0;i<70;i++)
+      System.out.print(" ");
+    System.out.println("Welcome to Pacement Cell");
+    System.out.println("Choices : ");
+    System.out.println("1 --> Sign Up");
+    System.out.println("2 --> Sign In");
+    System.out.println("\n*Enter -999 to close the application at any stage");
+    System.out.println("____________________________________________________________________________________________________________________________________________________________________________");
+  }
+}
