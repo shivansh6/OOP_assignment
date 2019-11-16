f6236a1b6647d56f1ceb34abdf2eae92c363cd23
shivansh6 <51433148+shivansh6@users.noreply.github.com>
2019-11-16 14:23:45 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 public class HomePage
 {
-  public static void main(String args[])
+  public static void HOMEPAGE()
   {
     System.out.println("____________________________________________________________________________________________________________________________________________________________________________");
     for(int i=0;i<75;i++)
@@ -13,6 +14,47 @@ public class HomePage
     System.out.println("1 --> Sign Up");
     System.out.println("2 --> Sign In");
     System.out.println("\n*Enter -999 to close the application at any stage");
+    System.out.print("Enter your choice :- ");
+    Scanner sc = new Scanner(System.in);
+    boolean ok = true;
+     while(ok)
+     {
+       int ch=98;
+       boolean flag1 = false;
+       do{
+         try{
+         ch = sc.nextInt();
+         flag1 =true;
+         }
+         catch(Exception e)
+         {
+           System.out.println("Enter valid choice");
+         }
+      }while(!flag1);
+      switch(ch)
+      {
+        case 1 :
+          SignUp ob = new SignUp();
+          ob.SIGNUP();
+          ok = false;
+          break;
+        case 2 :
+          Login ob1 = new Login();
+          ob1.Final_login();
+          ok = false;
+          break;
+        case -999 :
+          System.exit(0);
+        default :
+          System.out.println("Enter valid choice");
+      }
+    }
     System.out.println("____________________________________________________________________________________________________________________________________________________________________________");
+  }
+  public static void main(String args[])
+  {
+    //Login1 ob = new Login1(2);
+    //ob.makeHashMap();
+    HOMEPAGE();
   }
 }
